6971b5ca39de842c5ba0cd1eda7c0ebe8e666862
jonlouey <louey.jonathan@gmail.com>
2019-10-02 16:13:59 -0400

    more css

--- module-three-frontend/src/index.js
+++ module-three-frontend/src/index.js
@@ -24,38 +24,34 @@ function renderGame(){
     gameContainer.className = "game-container"
     const string = `
     <div id="game-title">
-    <h1>Do you think you are smart?</h1>
+    <h1>So, you think you are smart?</h1>
     </div>
     <div id="game-card">
-    <div id="game-question-title"><h3>What is capital of...</h3></div>   
-    <div id="game-question"></div>
-    <div id="game-question-2"></div>
-    <div id="game-question-3"></div>
-    <div id="game-question-4"></div>
+    <div id="game-question-title"><h3>What is the capital of...</h3></div> 
+    <div id="game-question"><ul id="game-question-ul"></ul></div>
     <div id="game-form">
         <form id="game-submission">
             <input type="text" id="user-input">
             <input type="submit" id="user-submit">
         </form>
     </div>
+    <div id="game-instructions"><p>Instructions: List all of the state capitals before the timer runs out. The timer will start after you submit the first capital. If you submit a correct capital, the state will be removed from the list. You do not need to list the capitals in order.</p></div>
     </div>
     <div id="game-sidebar">
     <div id="game-timer">
-        Timer
+        <h3>Timer<h3>
     </div>
 
     <div id="game-score">
-        <span id=score-title>Score</span>
-        <br>
-        <br>
+        <span id=score-title><h3>Score</h3></span>
         <br>
         <span id=points>0 Points</span>
     </div>
 
     <div id="game-highscore">
-        High Scores
+        <h3>High Scores</h3>
     </div>
-    </div>
+    </div>  
     `
     gameContainer.innerHTML = string
     body.append(gameContainer)
@@ -104,43 +100,43 @@ function populateStates(){
 
 populateStates()
 
-// function populateQuestion(){
-//     for (let capital in states) {
-//         document.querySelector("#game-question").insertAdjacentHTML("beforeEnd",
-//         `<li data-id=${states[capital][1]}>${states[capital][0]}</li>
-//         `)
-//       }
-// }
-
 function populateQuestion(){
-var keys = Object.keys(states);
-
-for (var i=1; i< 13; i++) { 
-    document.querySelector("#game-question").insertAdjacentHTML("beforeEnd",
-        `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
+    for (let capital in states) {
+        document.querySelector("#game-question-ul").insertAdjacentHTML("beforeEnd",
+        `<li data-id=${states[capital][1]}>${states[capital][0]}</li>
         `)
- }
+      }
+}
 
- for (var i=14; i< 26; i++) { 
-    document.querySelector("#game-question-2").insertAdjacentHTML("beforeEnd",
-        `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
-        `)
- }
+// function populateQuestion(){
+// var keys = Object.keys(states);
 
- for (var i=27; i< 39; i++) { 
-    document.querySelector("#game-question-3").insertAdjacentHTML("beforeEnd",
-        `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
-        `)
- }
+// for (var i=1; i< 13; i++) { 
+//     document.querySelector("#game-question").insertAdjacentHTML("beforeEnd",
+//         `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
+//         `)
+//  }
 
- for (var i=40; i< 50; i++) { 
-    document.querySelector("#game-question-4").insertAdjacentHTML("beforeEnd",
-        `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
-        `)
- }
+//  for (var i=14; i< 26; i++) { 
+//     document.querySelector("#game-question-2").insertAdjacentHTML("beforeEnd",
+//         `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
+//         `)
+//  }
+
+//  for (var i=27; i< 39; i++) { 
+//     document.querySelector("#game-question-3").insertAdjacentHTML("beforeEnd",
+//         `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
+//         `)
+//  }
+
+//  for (var i=40; i< 50; i++) { 
+//     document.querySelector("#game-question-4").insertAdjacentHTML("beforeEnd",
+//         `<ul data-id=${states[keys[i]][1]}>${states[keys[i]][0]}</ul>
+//         `)
+//  }
 
 
-}   
+// }   
 
 
 function incrementPoints(){
@@ -148,3 +144,7 @@ function incrementPoints(){
     let newScore = parseInt(score.innerText) + 1
     score.innerText = newScore + " Points"
 }
+
+
+
+
