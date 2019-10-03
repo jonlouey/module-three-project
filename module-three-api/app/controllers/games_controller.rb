f9888310c924d1360bcbebfe3dda434150ecd8d1
danielahn <93danielahn@gmail.com>
2019-10-03 11:29:57 -0400

    Change Games Controller

--- module-three-api/app/controllers/games_controller.rb
+++ module-three-api/app/controllers/games_controller.rb
@@ -2,11 +2,7 @@ class GamesController < ApplicationController
     def index
         games = Game.all.sort_by{ |game| -game.score }
         games = games.map do |game|
-            obj = {
-                name: game.user.name,
-                score: game.score
-            }
-            obj
+            { name: game.user.name, score: game.score }
         end
         render json: games.slice(0, 3), status: "200"
     end
